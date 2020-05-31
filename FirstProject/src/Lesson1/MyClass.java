8e872967074c5cfb78c0dcdcd5e4ec4f640f7151
ARVIND <ARVIND@DESKTOP-35EP390>
2020-05-31 19:24:25 +0530

    Commented stu2

--- FirstProject/src/Lesson1/MyClass.java
+++ FirstProject/src/Lesson1/MyClass.java
@@ -7,7 +7,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		Student stu1 = new Student();
 		stu1.setId(5);
-		Student stu2 = new Student(10001,"Arvind", 30);		
+		//Student stu2 = new Student(10001,"Arvind", 30);		
 		System.out.println(stu1.getId());
 	}
 }
